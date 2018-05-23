94ada85840fd4d6bf1eb22372889e477c878dbc3
Anish Karandikar <anishkny@gmail.com>
2018-05-23 17:55:01 -0400

    chore(ci): Add script to autogenerate repo listing in README

---
+++ etc/push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+set -e
+
+## Change into this script's folder
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+node ./update-readme.js
+
+git --version
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git checkout -b next
+git add README.md
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git remote add origin-next https://${GH_TOKEN}@github.com/anishkny/realworld.git
+git push --quiet --set-upstream origin-next next
---
+++ etc/update-readme.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+const fs = require('fs');
+const jsYaml = require('js-yaml');
+
+const README_TEMPLATE_FILE = 'README.tmpl.md';
+const README_TARGET_FILE = '../README.md';
+const FRONTEND_PLACEHOLDER = 'INSERT_FRONTEND_REPOS';
+const BACKEND_PLACEHOLDER = 'INSERT_BACKEND_REPOS';
+const FRONTEND_REPOS = jsYaml.safeLoad(fs.readFileSync('frontend-repos.yaml', 'utf8'));
+const BACKEND_REPOS = jsYaml.safeLoad(fs.readFileSync('backend-repos.yaml', 'utf8'));
+
+axios.defaults.headers.common['Authorization'] = `token ${process.env.GH_TOKEN}`;
+
+(async () => {
+  await main();
+})();
+
+async function main() {
+
+  const input = fs.readFileSync(README_TEMPLATE_FILE, 'utf8').split("\n");
+  const output = [];
+  for (let i = 0; i < input.length; ++i) {
+    if (input[i].indexOf(FRONTEND_PLACEHOLDER) > -1) {
+      output.push(...(await getSortedTable(FRONTEND_REPOS)));
+    } else if (input[i].indexOf(BACKEND_PLACEHOLDER) > -1) {
+      output.push(...(await getSortedTable(BACKEND_REPOS)));
+    } else {
+      output.push(input[i]);
+    }
+  }
+  fs.writeFileSync(README_TARGET_FILE, output.join('\n'));
+  console.log(`Wrote output to file: [${README_TARGET_FILE}]`);
+
+}
+
+async function getSortedTable(repos) {
+
+  // Get sorted repos by stargazers_count
+  for (let i = 0; i < repos.length; ++i) {
+    const stargazers_count =
+      (await axios.get(`https://api.github.com/repos/${repos[i].repo}`))
+      .data.stargazers_count;
+    repos[i].stargazers_count = stargazers_count;
+  }
+  repos = repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+
+  // Output sorted table
+  const output = [
+    '| 🥇 | 🥈 | 🥉 |',
+    '| :---:         |     :---:      |          :---: |',
+  ];
+  let string = '';
+  for (let i = 0; i < repos.length; ++i) {
+    string += `| [**${repos[i].title}**<br/> ` +
+      `![${repos[i].title}](${repos[i].logo}) ` +
+      `![Star](https://img.shields.io/github/stars/${repos[i].repo}.svg?style=social&label=Star) ` +
+      `![Fork](https://img.shields.io/github/forks/${repos[i].repo}.svg?style=social&label=Fork)]` +
+      `(https://github.com/${repos[i].repo})`;
+    if (!((i + 1) % 3)) {
+      output.push(string);
+      string = '';
+    }
+  }
+  output.push(string);
+  return output;
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at:', p, 'reason:', reason);
+});
