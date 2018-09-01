8547f1da42c32ab3fe8f70f089adf12d00ff1aac
Anish Karandikar <anishkny@gmail.com>
2018-09-01 17:58:18 -0400

    fix(API): A few fixes to Postman collection (#255)
    * fix(API): A few fixes to Postman collection
    
    * fix(API): Make API tests self contained, remove cruft
    
    * Update README.md

---
+++ api/run-api-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -x
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+APIURL=${APIURL:-https://conduit.productionready.io/api}
+USERNAME=${USERNAME:-u`date +%s`}
+EMAIL=${EMAIL:-$USERNAME@mail.com}
+PASSWORD=${PASSWORD:-password}
+
+npx newman run $SCRIPTDIR/Conduit.postman_collection.json \
+  --delay-request 500 \
+  --global-var "APIURL=$APIURL" \
+  --global-var "USERNAME=$USERNAME" \
+  --global-var "EMAIL=$EMAIL" \
+  --global-var "PASSWORD=$PASSWORD"
