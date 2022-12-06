279e2437dfb7cd9d334cc3fa8017a32306e12ce2
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-12-06 15:52:16 +0100

    chore: nx init

---
+++ apps/swagger/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="swagger" />
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ apps/swagger/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+import App from './app/app';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
