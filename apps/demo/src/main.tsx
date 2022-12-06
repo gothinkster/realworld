fe46d023dc6078c1433df7dc0cd1e7ea5277eb48
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-12-06 19:43:46 +0100

    feat(demo): add project

---
+++ apps/demo/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './app/app';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
