ba47c60329c3dd067bd942a7c8461c4e7c2c7037
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-12-09 18:26:17 +0100

    feat(demo): add storybook

---
+++ apps/demo/.storybook/main.js
@@ -0,0 +1,12 @@
+const rootMain = require('../../../.storybook/main');
+
+module.exports = {
+  ...rootMain,
+  core: { ...rootMain.core, builder: '@storybook/builder-vite' },
+  stories: [
+    ...rootMain.stories,
+    '../src/app/**/*.stories.mdx',
+    '../src/app/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [...rootMain.addons],
+};
