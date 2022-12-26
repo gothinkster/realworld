95a2c62ec083b2ed522a60d4536f43866e213bf4
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-12-26 23:15:36 +0100

    feat(design-system): add pseudo states

---
+++ apps/demo/src/app/stories/atoms/button/button.utils.tsx
@@ -0,0 +1,24 @@
+const code = (theme: string) => {
+  const classes = `rl-btn rl-btn-${theme}`;
+  return <button className={classes}>Submit</button>;
+};
+
+const showcaseCode = (theme: string) => `<button class="rl-btn rl-btn-${theme}">Submit</button>`;
+
+const docs = (theme: string) => ({
+  docs: {
+    source: {
+      dark: true,
+      code: showcaseCode(theme),
+      language: 'html',
+    },
+  },
+});
+
+export const primaryButton = code('primary');
+export const secondaryButton = code('secondary');
+export const warnButton = code('warn');
+
+export const primaryButtonDocs = docs('primary');
+export const secondaryButtonDocs = docs('secondary');
+export const warnButtonDocs = docs('warn');
