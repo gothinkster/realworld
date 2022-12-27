5d35049577ce45206015115e6b42fdeee0f71acd
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-12-27 11:30:41 +0100

    feat(design-system): add upload file examples

--- apps/demo/src/app/stories/molecules/upload.stories.tsx
+++ apps/demo/src/app/stories/molecules/upload.stories.tsx
@@ -1,15 +1,37 @@
+import coverImage from '../assets/end.png';
+
 export default {
   title: 'Molecules/Input File',
 };
 
 export const InputFile = () => (
   <>
-    <fieldset className="rl-form-group--file">
-      <label>Choose a cover image</label>
+    <fieldset className="rl-file-form-group">
+      <label>Cover image</label>
+      <div className="rl-file-form-group__input">
+        <div>Drag and drop the file here</div>
+        <div>- OR -</div>
+        <button type="button" className="rl-btn rl-btn-secondary">
+          Browse files
+        </button>
+      </div>
       <input type="file" className="rl-input-file" />
-      <button type="button" className="rl-btn rl-btn-secondary">
-        Upload
-      </button>
+    </fieldset>
+
+    <fieldset className="rl-file-form-group">
+      <label>Cover image</label>
+      <div className="rl-file-form-group__input">
+        <img className="rl-file-form-group__preview" src={coverImage} alt="" />
+        <div className="rl-file-form-group__button-container">
+          <button type="button" className="rl-btn rl-btn-secondary">
+            Browse files
+          </button>
+          <button type="button" className="rl-btn rl-btn-warn">
+            Remove
+          </button>
+        </div>
+      </div>
+      <input type="file" className="rl-input-file" />
     </fieldset>
   </>
 );
