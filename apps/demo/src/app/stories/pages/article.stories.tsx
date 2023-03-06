004cd02b90653bcf37bb7222acf703fce7001ebb
geromegrignon <gerome.grignon.lp2@gmail.com>
2023-03-06 18:31:17 +0100

    feat(storybook): add icon to like button

--- apps/demo/src/app/stories/pages/article.stories.tsx
+++ apps/demo/src/app/stories/pages/article.stories.tsx
@@ -46,7 +46,12 @@ export const Article = () => (
           </p>
         </hgroup>
         <img className="rw-article-header__cover" src="avatar.png" alt="user avatar" />
-        <button className="rw-btn-secondary rw-btn-follow">Like me</button>
+        <button className="rw-btn-secondary rw-btn-follow">
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
+            <path d="M720 912H264V432l288-288 32 22q17 12 26 30.5t5 38.5l-1 5-38 192h264q30 0 51 21t21 51v57q0 8-1.5 14.5T906 589L786.93 868.199Q778 888 760 900t-40 12Zm-384-72h384l120-279v-57H488l49-243-201 201v378Zm0-378v378-378Zm-72-30v72H120v336h144v72H48V432h216Z" />
+          </svg>{' '}
+          <span>Like me</span>
+        </button>
       </section>
       <ul className="rw-tag-list-horizontal rw-article-header__tags">
         <li className="rw-tag">
