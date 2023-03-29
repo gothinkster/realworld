b6c68838da4c929faa162791b18a808e4b8d9075
geromegrignon <gerome.grignon.lp2@gmail.com>
2023-03-29 18:52:02 +0200

    feat(api-testing-cypress): add tag testing

--- apps/api-testing-cypress/src/support/e2e.ts
+++ apps/api-testing-cypress/src/support/e2e.ts
@@ -12,6 +12,7 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
+import '@bahmutov/cy-api';
 
 // Import commands.js using ES2015 syntax:
 import './commands';
