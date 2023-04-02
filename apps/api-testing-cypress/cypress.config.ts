4d6f5e072f521a8c4a55dd70ca1f606910ee33d7
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-04-02 17:16:39 +0200

    feat(api-testing-cypress): add Cypress Cloud id

--- apps/api-testing-cypress/cypress.config.ts
+++ apps/api-testing-cypress/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
+  projectId: "ss7tiw",
   e2e: nxE2EPreset(__dirname, {
     // TODO : move to vite once fixed (https://github.com/nrwl/nx/issues/15890)
     bundler: 'webpack',
