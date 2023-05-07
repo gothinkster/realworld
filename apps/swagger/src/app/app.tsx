383eebd4b519b6c383152366d7ed99c025c982e2
geromegrignon <gerome.grignon.lp2@gmail.com>
2023-05-07 10:44:07 +0200

    refactor(documentation): move swagger ui to documentation

--- apps/swagger/src/app/app.tsx
+++
@@ -1,7 +0,0 @@
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-import swaggerDocument from '../assets/swagger.json';
-
-export const App = () => <SwaggerUI spec={swaggerDocument} />;
-
-export default App;
