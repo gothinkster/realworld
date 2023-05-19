3f1f55eb1cb4d0a2e5a076b30308c13c62149b56
geromegrignon <gerome.grignon.lp2@gmail.com>
2023-05-19 18:54:14 +0200

    refactor(demo): remove react demo app

--- apps/demo/.storybook/main.js
+++
@@ -1,9 +0,0 @@
-const rootMain = require('../../../.storybook/main');
-
-module.exports = {
-  ...rootMain,
-  core: { ...rootMain.core, builder: '@storybook/builder-vite' },
-  stories: [...rootMain.stories, '../src/app/**/*.mdx', '../src/app/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', ...rootMain.addons],
-  staticDirs: ['../public'],
-};
--- apps/demo/src/app/stories/atoms/title/title.stories.tsx
+++
@@ -1,7 +0,0 @@
-export default {
-  title: 'Atoms/Title',
-};
-
-export const TitleDefault = {
-  render: () => <h1 className="rw-page-title">Create account</h1>,
-};
