04fcd191edc9acbb219984434071b4c9bf43978d
geromegrignon <gerome.grignon.lp2@gmail.com>
2023-06-25 19:24:17 +0200

    fix(api): heroku web command

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,7 +14,7 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
