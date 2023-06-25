5f903084f9010d95d545b0c5ac4524dd91913e71
geromegrignon <gerome.grignon.lp2@gmail.com>
2023-06-25 21:15:35 +0200

    fix(api): move index to main.js

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,60 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
-import * as path from 'path';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+import routes from './app/routes/routes';
+import { generateFakeData } from './app/utils/cron';
+import HttpException from './app/models/http-exception.model';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+/**
+ * App Configuration
+ */
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
+
+// Serves images
+app.use(express.static('public'));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.json({ status: 'API is running on /api' });
 });
 
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+/* eslint-disable */
+app.use(
+  (
+    err: Error | HttpException,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    // @ts-ignore
+    if (err && err.name === 'UnauthorizedError') {
+      return res.status(401).json({
+        status: 'error',
+        message: 'missing authorization credentials',
+      });
+      // @ts-ignore
+    } else if (err && err.errorCode) {
+      // @ts-ignore
+      res.status(err.errorCode).json(err.message);
+    } else if (err) {
+      res.status(500).json(err.message);
+    }
+  },
+);
+
+generateFakeData();
+
+/**
+ * Server activation
+ */
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.info(`server up on port ${PORT}`);
 });
-server.on('error', console.error);
