5f53c4d609b8422177d3678dcbb6107d74876c90
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2023-10-26 18:09:36 +0500

    fix code that broke deployment

--- apps/api/src/app/models/http-exception.model.ts
+++ apps/api/src/app/models/http-exception.model.ts
@@ -1,10 +1,9 @@
 class HttpException extends Error {
   errorCode: number;
-
   constructor(
     errorCode: number,
-    public readonly message: string,
-  ) {
+    public readonly message: string | any
+    ) {
     super(message);
     this.errorCode = errorCode;
   }
